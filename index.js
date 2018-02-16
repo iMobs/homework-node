c21f41afda7c3bd064e6ee711204fa1b5b5e8596
Ian Mobley <ianmobley@gmail.com>
2018-02-16 13:08:52 -0800

    Hard code top 10 and add download helper function
    I'm having a hard time finding an API endpoint that will list
    out packages ordered by most depended upon. For now it is just hard
    coded at 10 and the helper function takes the package name, looks up its
    info in the registry, and downloads the latest tarball.

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 'use strict'
 
-module.exports = downloadPackages
+const axios = require('axios');
+const download = require('download');
+const Promise = require('bluebird');
 
-function downloadPackages (count, callback) {
+// Hard Code the packages for now
+const packages = [
+  'lodash',
+  'request',
+  'chalk',
+  'async',
+  'express',
+  'bluebird',
+  'react',
+  'commander',
+  'debug',
+  'moment'
+]
 
+module.exports = downloadPackages;
+
+async function downloadPackages (count, callback) {
+  await Promise.map(packages, downloadPackage);
+
+  // Finally invoke the callback to signal we're done
+  if (typeof callback === 'function') {
+    callback();
+  }
 }
+
+const downloadPackage = async (packageName) => {
+  const { data } = await axios.get(`https://registry.npmjs.com/${packageName}`);
+
+  const version = data['dist-tags'].latest;
+  
+  const latestVersion = data.versions[version];
+  
+  const { tarball } = latestVersion.dist;
+  
+  await download(tarball, `./packages/${packageName}`, {
+    extract: true,
+  });
+};
