cf8edc6246c344fba76a84b8740719fd03fad29b
Ian Mobley <ianmobley@gmail.com>
2018-02-16 18:38:45 -0800

    Remove bluebird and wrap in try/catch
    bluebird wasn't being heavily used so I don't think there's much point
    depending on it. The try/catch will handle any promise rejections or
    other problems.

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const del = require('del');
 const download = require('download');
-const Promise = require('bluebird');
 const fs = require('fs-extra');
 
 module.exports = async (count, callback) => {
-  await del('./packages/*');
-
-  const packages = await getPackageList(count);
-
-  await Promise.map(packages, downloadPackage);
-
-  // Finally invoke the callback to signal we're done
-  if (typeof callback === 'function') {
-    callback();
+  try {
+    await del('./packages/*');
+  
+    const packages = await getPackageList(count);
+  
+    await Promise.all(packages.map(downloadPackage));
+  
+    // Finally invoke the callback to signal we're done
+    if (typeof callback === 'function') {
+      callback();
+    }
+  } catch (error) {
+    console.error('Failed to download packages:', error.message);
   }
 };
 
@@ -65,12 +68,14 @@ const downloadPackage = async (packageName) => {
   });
 
   // Gotta move the contents of $packageName/package up
-  await Promise.map(fs.readdir(`${packagePath}/package`), file => {
+  const contents = await fs.readdir(`${packagePath}/package`);
+
+  await Promise.all(contents.map(file => {
     const oldPath = `${packagePath}/package/${file}`;
     const newPath = `${packagePath}/${file}`;
 
     return fs.move(oldPath, newPath);
-  });
+  }));
 
-  await fs.remove(`${packagePath}/package`);
+  fs.remove(`${packagePath}/package`);
 };
