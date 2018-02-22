8218ff6ca8d087cae1f1b4f2d8a83cf47e62c6f5
Ian Mobley <ianmobley@gmail.com>
2018-02-22 11:12:34 -0800

    Add some more comments
    Explain the thought process and what certain sections are doing.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const fs = require('fs-extra');
 
 module.exports = async (count, callback) => {
   try {
+    // Clean out old packages
     await del('./packages/*');
 
     const packages = await getPackageList(count);
@@ -19,6 +20,7 @@ module.exports = async (count, callback) => {
     }
   } catch (error) {
     console.error('Failed to download packages:', error.message);
+    callback(error);
   }
 };
 
@@ -26,14 +28,15 @@ const getPackageList = async (count) => {
   const packages = [];
 
   while (packages.length < count) {
-    // Get the most-depended-upon page
+    // Get the most-depended-upon page (there is no API endpoint)
+    // https://github.com/npm/www/issues/154
     const { data } = await axios.get('https://www.npmjs.com/browse/depended', {
       params: {
         offset: packages.length,
       },
     });
 
-    // Load it into the parser
+    // Load it into the parser so it can be easily searched
     const $ = cheerio.load(data);
 
     // Select all package detail names (easiest place to find what we want)
@@ -52,27 +55,31 @@ const getPackageList = async (count) => {
 };
 
 const downloadPackage = async packageName => {
+  // Get package data from the registry
   const { data: packageData } = await axios.get(
     // Slashes must be escaped
     `https://registry.npmjs.com/${packageName.replace(/\//g, '%2F')}`
   );
+
+  // Based on the latest version of the package, grab the tarball url
   const version = packageData['dist-tags'].latest;
   const latestVersion = packageData.versions[version];
   const { tarball } = latestVersion.dist;
   const packagePath = `./packages/${packageName}`;
 
+  // Download the tarball and automatically extract it at `packagePath`
   await download(tarball, packagePath, {
     extract: true,
   });
 
-  /* All of the tarballs can be assumed to have a single root directory.
+  /* All of the packages can be assumed to have a single root directory.
    * This will usually just be a folder called package.
    * Ejs seems to be the exception, which is why this needs to be read.
    */
   const [ subdir ] = await fs.readdir(packagePath);
   const contents = await fs.readdir(`${packagePath}/${subdir}`);
 
-  // Move all of the package contents up a directory level
+  // Move all of the package contents up a directory
   await Promise.all(contents.map(file => {
     const oldPath = `${packagePath}/${subdir}/${file}`;
     const newPath = `${packagePath}/${file}`;
